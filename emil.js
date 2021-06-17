bfe2066202d2e4325b768f708e3d0f1e975a45c4
Ashish Narayan Shinde <ashishshinde765@gmail.com>
2021-06-17 19:53:55 +0530

    Initial commit

---
+++ emil.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#EML").click(function(){
+        $(".dt").toggle();
+        $(".dtt").hide();
+
+      //  alert('hi');
+    })
+    $("#MBL").click(function(){
+        $(".dtt").toggle();
+        $(".dt").hide();
+      //  alert('hi');
+    })
+})
